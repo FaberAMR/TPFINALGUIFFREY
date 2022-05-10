82d71acbfc41c4aeba5b6c66cfb3492a8c7a6998
Fabri Guiffrey <amrcdelu@gmail.com>
2022-05-10 04:57:59 -0300

    tegundo commit

--- proyectoluna/appAdopciones/models.py
+++ proyectoluna/appAdopciones/models.py
@@ -1,4 +1,5 @@
 from email.mime import image
+from tabnanny import verbose
 from django.db import models
 
 
@@ -9,9 +10,13 @@ class Pet(models.Model):
     fecha_nac=models.DateTimeField() #fecha
     datos=models.CharField(max_length=40) #subtutlo
     imagen=models.ImageField(upload_to='appAdopciones') #imagen
-    # contacto=models.EmailField() #autor
+    #contacto=models.IntegerField() #autor
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='Adopcion'
+        verbose_name_plural='Adopciones'
     
     def __str__(self):
         return self.nombre
